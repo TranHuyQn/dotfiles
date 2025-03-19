219acf6fcb1f6d1bd057c4eb51a5bf4400a82fb3
HuyTV <tranvanhuy0202@gmail.com>
2025-03-20 03:01:16 +0700

    config neovim

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -9,7 +9,7 @@ config.window_decorations = 'RESIZE'
 config.color_scheme = 'Tokyo Night'
 config.hide_tab_bar_if_only_one_tab = true 
 config.font = wezterm.font 'Hack Nerd Font'
-config.font_size = 13
+config.font_size = 14 
 config.window_background_opacity = 0.9
 config.macos_window_background_blur = 30
 
